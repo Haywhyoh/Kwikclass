35e966685f76c377982de9343734aef288210e86
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-11-15 10:44:45 +0100

    get courses by instructor fixed

--- api/views/instructor.py
+++ api/views/instructor.py
@@ -1,6 +1,6 @@
 from models.engine.db_storage import DB_Storage
 from models.models import Instructor
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort
 from flask import Blueprint
 import json
 
@@ -8,7 +8,7 @@ instructor = Blueprint('instructor', __name__)
 storage = DB_Storage()
 
 @instructor.route('/instructors', strict_slashes=False)
-def get_instructor():
+def get_instructors():
     new_list = []
     all_instructors = storage.all(cls=Instructor)
     for instructor in all_instructors:
@@ -17,3 +17,13 @@ def get_instructor():
             del instructor['_sa_instance_state']
         new_list.append(instructor)
     return jsonify((new_list))
+
+@instructor.route('/instructors/<instructor_id>', strict_slashes=False)
+def get_instructor(instructor_id):
+    """ Retrieves an instructor """
+    instructor = storage.get(cls=Instructor, id=instructor_id)
+    if not instructor:
+        abort(404)
+    return jsonify(instructor)
+
+
