548f9ab88ed99cbe867ec7d64a7e881ec34ff48a
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-11-15 17:09:59 +0100

    put and delete for courses

--- api/views/courses.py
+++ api/views/courses.py
@@ -1,6 +1,6 @@
 from models.engine.db_storage import DB_Storage
 from models.models import Courses
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, make_response, request
 from flask import Blueprint
 import json
 
@@ -26,4 +26,32 @@ def get_course(course_id):
         abort(404)
     return jsonify(course)
 
+@courses.route('/courses/<courses_id>', methods=['DELETE'], strict_slashes=False)
+def delete_course(course_id):
+        """
+        Deletes a user Object
+        """
+        course = storage.local_session.query(course).filter_by(id=course_id).delete(synchronize_session=False)
+        if not course:
+            abort(404)
+        storage.save()
+        return make_response(jsonify({'status' : "Succesfully deleted"}), 200)
+
+@courses.route('/courses', methods=['POST'], strict_slashes=False)
+def post_course():
+    """
+    Creates a course
+    """
+    if not request.get_json():
+        abort(400, description="Not a JSON")
+
+    if 'email' not in request.get_json():
+        abort(400, description="Missing email")
+    if 'password' not in request.get_json():
+        abort(400, description="Missing password")
+
+    data = request.get_json()
+    instance = Courses(**data)
+    instance.save()
+    return make_response(jsonify(instance.to_dict()), 201)
 
