e7b88402feb8667fd4b4508063b501ec18514ee1
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-11-15 10:44:54 +0100

    get student by id fixed

--- api/views/student.py
+++ api/views/student.py
@@ -1,7 +1,6 @@
 from models.engine.db_storage import DB_Storage
 from models.models import Student
-from api.views import app_views
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort
 from flask import Blueprint
 import json
 
@@ -9,12 +8,23 @@ student = Blueprint('student', __name__)
 storage = DB_Storage()
 
 @student.route('/students', strict_slashes=False)
-def get_student():
-    new_list = {}
+def get_students():
+    new_list = []
     all_students = storage.all(cls=Student)
     for student in all_students:
         student = student.__dict__ 
         if student['_sa_instance_state']:
             del student['_sa_instance_state']
-        new_list.update(student)
+        new_list.append(student)
     return jsonify((new_list))
+
+@student.route('/students/<student_id>', strict_slashes=False)
+def get_student(student_id):
+    """ Retrieves an user """
+    student = storage.get(cls=Student, id=student_id)
+    if not student:
+        abort(404)
+    return jsonify(student)
+
+    
+
