ceef0d864ac636b871f47905f551e90b60d439f3
Adedayo Ayomide <adefeyisayo998@gmail.com>
2022-11-15 08:41:42 +0100

    registred courses and instructors blueprint

--- api/app.py
+++ api/app.py
@@ -2,16 +2,21 @@ from flask import Flask
 from api.views import app_views
 from flask import jsonify, make_response
 from models.engine.db_storage import DB_Storage
+from api.views.student import student
+from api.views.courses import courses
+from api.views.instructor import instructor
+storage = DB_Storage()
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+app.register_blueprint(student, url_prefix="/api")
+app.register_blueprint(courses, url_prefix="/api")
+app.register_blueprint(instructor, url_prefix="/api")
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
 
 @app.teardown_appcontext
 def close_db(error):
-    DB_Storage.close()
+    storage.close()
 
 @app.route('/status')
 def status():
